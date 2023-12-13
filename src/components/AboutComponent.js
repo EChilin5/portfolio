1ed4d98703f13446aecf4b782dddb72f01bcce25
EChilin5 <edgar3ac@gmail.com>
2023-12-13 14:16:32 -0800

    update about section

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./AboutComponent.css";
+
 import CardComponent from "./CardComponent";
 import reactImage from "../image/react.png";
 import css from "../image/CSS.png";
@@ -8,75 +8,60 @@ import java from "../image/Java.png";
 import kotlin from "../image/kotlin.png";
 import sql from "../image/SQL.png";
 
-function About() {
-  return (
-    <div className="about">
-      <div className="about-left">
-        <div className="about-left-wrapper">
-          <h2>About Me</h2>
-          <hr />
-          <p>
-            I am a developer interested in both web and mobile development. I
-            have a passion for creating applications that can help benefit my
-            community. I have recently launched three applications on the Google
-            Play store to help with productivity and health that are currently
-            listed in my project section. My overall goal is to create an app
-            that can have a large impact on people or contribute to a
-            large-scale application that a variety of different people use.
-          </p>
-          <p>
-            Additionally, I have IT Experience as I have previously helped set
-            up and troubleshoot both Windows and Mac devices. If you have any
-            issues feel free to reach out to see if I can assist you.
-          </p>
-        </div>
-      </div>
+import "./AboutComponent.css";
 
-      <div className="about-right">
-        <h2>Skills</h2>
-        <div className="bg-grid">
-          <div className="grid">
-            <CardComponent
-              className="column"
-              image={reactImage}
-              title="React"
-              description=""
-            />
-            <CardComponent
-              className="column"
-              image={css}
-              title="CSS"
-              description=""
-            />
-            <CardComponent
-              className="column"
-              image={kotlin}
-              title="Kotlin"
-              description=""
-            />
-            <CardComponent
-              className="column"
-              image={firestore}
-              title="FireStore"
-              description=""
-            />
-            <CardComponent
-              className="column"
-              image={java}
-              title="Java"
-              description=""
-            />
-            <CardComponent
-              className="column"
-              image={sql}
-              title="SQL"
-              description=""
-            />
+const AboutComponent = () => {
+  return (
+    <div>
+      <div className="about-container">
+        <div className="about-container-right">
+          <img src={reactImage} alt="testing will be replaced" />
+        </div>
+        <div className="about-container-left">
+          <h2>About Me</h2>
+          <p>
+            Hello, I'm Edgar Chilin, a dedicated software developer with a
+            passion for both mobile and web development. My mission is to create
+            applications that address real-world challenges and enhance people's
+            daily lives.{" "}
+          </p>
+          <p>
+            Launched a few mobile applications on the Play Store using Kotlin
+            and Firebase, I've honed my skills in crafting innovative solutions.
+            One of my most rewarding projects is a nutrition-based application
+            to keep records of your weight loss journey, a food journal, and a
+            variety of recipes from the Edaman API.
+          </p>
+          <p>
+            In the realm of web development, I'm excited to announce that my
+            upcoming websites, built with React JS, C#, and SQL, are just around
+            the corner. These platforms are geared towards assisting businesses
+            in selling their product, allowing users to make appointments, and
+            providing users with a healthier lifestyle.
+          </p>
+
+          <div className="about-tech-stack">
+            <p>Technologies I am familar with</p>
+            <ul>
+              <div className="about-tech-stack-container">
+                <div className="about-tech-stack-left">
+                  <li>REACT JS</li>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>C#</li>
+                </div>
+                <div className="about-tech-stack-right">
+                  <li>SQL</li>
+                  <li>Kotlin</li>
+                  <li>Java</li>
+                </div>
+              </div>
+            </ul>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default About;
+export default AboutComponent;
